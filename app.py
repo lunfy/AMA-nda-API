61aae0d6fbd0d53adde2d5ba4c8bc8db87bcce7e
lunfy <lunfy.aman@gmail.com>
2022-11-02 02:33:40 +0800

    completed JWT verification wrapper to protect routes

--- app.py
+++ app.py
@@ -3,19 +3,44 @@ import psycopg2
 import postgres
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import firebase_admin
+from firebase_admin import credentials, auth
+from functools import wraps
+
 
 load_dotenv()  # loads variables from .env file into environment
 
 app = Flask(__name__)
+
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+
+cors = CORS(app)
 url = os.environ.get("DATABASE_URL")  # gets variables from environment
-auth = os.environ.get("FIREBASE_AUTH")
 connection = psycopg2.connect(url)
 
-@app.before_request
-def before_request():
-    authKey = request.form['authKey']
-    if (authKey != auth):
-        return {"message": "Unauthorized access, invalid authentication details."}, 401
+# @app.before_request
+# def before_request():
+#     authKey = request.json['authKey']
+#     if (authKey != auth):
+#         return {"message": "Unauthorized access, invalid authentication details."}, 401
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('Authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            print(request)
+            user = auth.verify_id_token(request.headers.get('Authorization'))
+            print('user: ',user)
+            uid = user['uid']
+        except:
+            print(request.headers.get('Authorization'))
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
 
 @app.route("/api/users/register", methods=['POST'])
 def user_registers():
@@ -33,6 +58,7 @@ def user_registers():
     return {"message": f"User {email} has been created with ID: {userid}"}, 201
 
 @app.route("/api/users", methods=['GET','POST', 'PUT'])
+@check_token
 def user_signin():
     if request.method == 'POST':
         email = request.form['email']
@@ -59,3 +85,6 @@ def user_signin():
 
     # Edit profile route with PUT
     # if request.method == 'PUT':
+
+# @app.route("/api/requests", methods=['GET','POST'])
+# def 
